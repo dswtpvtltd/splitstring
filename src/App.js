077189e99cf3e2e7a02de1d4efcb29fb6a0b1f82
anyboat <developer@anyboat.com.au>
2023-09-18 12:38:31 +0530

    deploy code on git

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ const hightlightWithLineNumbers = (input, language) =>
     .join("\n");
 
 function App() {
-  const [value, setValue] = useState("");
+  const [value, setValue] =
+    useState(`0x2CB99F193549681e06C6770dDD5543812B4FaFE8 10
+0xEb0D38c92deB969b689acA94D962A07515CC5204 5
+0xF4aDE8368DDd835B70b625CF7E3E1Bc5791D18C1 6
+0x09ae5A64465c18718a46b3aD946270BD3E5e6aaB 9
+0x8B3392483BA26D65E331dB86D4F430E9B3814E5e 10`);
 
   const valueToArray = () => {
     if (value === "") return [];
